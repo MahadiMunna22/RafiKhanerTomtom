e52ea01dfafb2e8ccaf907ce48d73cc27747364b
User <mahadimunna22@gmail.com>
2019-09-25 16:06:10 +0600

    Initial commit

---
+++ app/src/main/java/com/example/rafikhanertomtom/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.rafikhanertomtom;
+
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tomtom.online.sdk.map.MapFragment;
+import com.tomtom.online.sdk.map.OnMapReadyCallback;
+import com.tomtom.online.sdk.map.TomtomMap;
+import com.tomtom.online.sdk.map.style.layers.Layer;
+
+import java.util.List;
+
+public class MainActivity extends FragmentActivity  {
+    TomtomMap tomtomMap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MapFragment mapFragment = (MapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment);
+        mapFragment.getAsyncMap(onMapReadyCallback);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        tomtomMap.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private final OnMapReadyCallback onMapReadyCallback =
+            new OnMapReadyCallback() {
+                @Override
+                public void onMapReady(TomtomMap map) {
+                    //Map is ready here
+                    tomtomMap = map;
+                    tomtomMap.setMyLocationEnabled(true);
+                    //tomtomMap.collectLogsToFile(Sam.LOGCAT_PATH);
+                    tomtomMap.getTrafficSettings().turnOnVectorTrafficFlowTiles();
+                }
+            };
+
+
+}
